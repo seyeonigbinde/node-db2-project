430091dcb740a0632dd6cbaf501bbcc2e6026295
Your Name <you@example.com>
2021-06-09 00:05:08 +0100

    car model and router fixed

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,10 +1,39 @@
+const Car = require('../cars/cars-model')
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  Car.get(req.params.id)
+    .then(car => {
+      if (!car) {
+        res.status(404).json({
+          error: `car with id ${req.params.id} is not found`
+        })
+      } else {
+        req.cars = car
+        next()
+      }
+    })
+    .catch(err => {
+      next(err)
+    })
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { name , description} = req.body
+  if ( !name || !description ) {
+    // validation fails
+    next({
+      message: 'missing required name and description field',
+      status: 400,
+    })
+
+  } else {
+    req.projects = { name: req.body.name.trim() }
+    req.projects = { name: req.body.description.trim() }
+    next()
+    // validation succeed
+  }
 }
+
 
 const checkVinNumberValid = (req, res, next) => {
   // DO YOUR MAGIC
@@ -12,4 +41,11 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId,
+  checkCarPayload,
+  checkVinNumberUnique,
+  checkVinNumberValid
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,42 +1,39 @@
-const express = require('express');
-const db = require('../../data/db-config');
-const router = express.Router();
+const router = require('express').Router()
 
-router.get('/', (req, res) => {
-  db('cars')
-    .then(car => {
-      res.json(car);
-    })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve car', err: err.message });
-    });
-});
+const Car = require('./cars-model');
+const {
+    checkCarId,
+    checkVinNumberUnique,
+    checkCarPayload,
+    checkVinNumberValid
+  } = require('./cars-middleware');
 
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  db('cars').where({ id }).first()
-    .then(car => {
-      res.json(car);
+  router.get('/', (req, res, next) => {
+    Car.get(req.query.length)
+    .then(account => {
+      res.status(200).json(account);
     })
-    .catch(err => {
-      res.status(500).json({ message: 'Failed to retrieve car' });
-    });
-});
-
-router.post('/', (req, res) => {
-  const carData = req.body;
-  db('cars').insert(carData)
-    .then(ids => {
-      return db('cars').where({ id: ids[0] })
-    })
-    .then(newCarEntry => {
-      res.status(201).json(newCarEntry);
-    })
-    .catch(err => {
-      console.log('POST error', err);
-      res.status(500).json({ message: "Failed to store data" });
-    });
-});
+    .catch(next)
+  })
+  
+  router.get('/:id', checkCarId, 
+  (req, res, next) => {
+    res.json(req.account)
+  })
+  
+  router.post('/', checkCarPayload, 
+  checkVinNumberUnique, 
+  checkVinNumberValid, 
+  (req, res, next) => {
+    const postCar = { ...req.body};
+    Car.insert(postCar)
+      .then(car => {
+        res.status(201).json(car);
+      })
+      .catch(error => {
+        next(error)
+      });
+  })
+  
 
 module.exports = router;
